afe6c5fb3e335bf1a54113fc5b7908ead8833dcd
jhonysalice <jhonysalice93@gmail.com>
2022-07-06 10:00:58 -0300

    LandingSent style

--- pages/landingSent/index.tsx
+++ pages/landingSent/index.tsx
@@ -9,13 +9,13 @@ function LandingSent() {
   const { query } = useRouter();
 
   return (
-    <div className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden py-6 sm:py-12 bg-gradient-to-b from-[#00A099] h-full">
+    <div className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden py-6 sm:py-12 h-full">
       <div className="max-w-xl px-5 text-center">
         <div className="mb-6">
           <Image src={canadoctorsLogo} height={80} width={350} alt="" />
         </div>
         <div>
-          <Image src={iconEmail} height={200} width={200} alt="" />
+          <Image src={iconEmail} height={159} width={150} alt="" />
         </div>
         <h2 className="mt-8 text-[30px] font-semibold text-[#5A5A5A]">
           Gracias <span className="capitalize">{query.name}</span>, nos
