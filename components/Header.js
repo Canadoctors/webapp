1584a8efc0c4a025ac8929ed8565e4ca253f2e92
jhonysalice <jhonysalice93@gmail.com>
2022-07-07 18:35:08 -0300

    Firebase config

--- components/Header.js
+++ components/Header.js
@@ -8,8 +8,8 @@ import { MdOutlineAccountBalanceWallet } from "react-icons/md";
 const style = {
   wrapper: `absolute w-screen px-[1.2rem] py-[0.8rem] flex justify-between `,
   logoContainer: `flex items-center cursor-pointer`,
-  logoText: ` ml-[0.8rem] text-white font-semibold text-2xl`,
-  headerItems: ` flex float-right items-center justify-end`,
+  logoText: `ml-[0.8rem] text-white font-semibold text-2xl`,
+  headerItems: `flex float-right items-center justify-end`,
   headerItem: `text-white px-4 font-bold text-[#c8cacd] hover:text-white cursor-pointer`,
   headerIcon: `text-[#8a939b] text-3xl font-black px-4 hover:text-white cursor-pointer`,
 };
