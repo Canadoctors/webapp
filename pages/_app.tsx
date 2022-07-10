b44e38ae1cd010ff91b98481c99b52e8f9e67a13
fedebais <federicomatiasbais@gmail.com>
2022-07-10 01:18:45 -0300

    add GTAG

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,54 @@
 import "../styles/globals.css";
+import React, { useEffect } from 'react'; // <--- import the hook
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import {useRouter, Router} from 'next/router';
+
+declare global {
+  interface Window {
+    dataLayer: Record<string, any>[];
+  }
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  function pageView(url: string , title: string) {
+
+    window && window.dataLayer && window.dataLayer.push({
+      'event': 'virtualPageview',
+      'virtualPageURL': url,
+      'virtualPageTitle': title,
+  });
+
+
+    return (
+      console.log(title),
+      console.log(url)
+    )
+  }
+
+
+   
+
+  useEffect(() => {
+
+    pageView(router.pathname, document.title);
+
+    const handleRouteChange = (url:string) => {
+      pageView(url, document.title);
+  };
+  Router.events.on('routeChangeComplete', handleRouteChange);
+  return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange);
+  };
+}, []);
+
+
+
+   
+
+    
   return (
     <>
       <Head>
@@ -14,3 +60,5 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp;
+
+
