6c579de6b0bf4eded696d682e64dcb7162bc7446
jhonysalice <jhonysalice93@gmail.com>
2022-07-11 10:16:06 -0300

    Code refactoring

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import "../styles/globals.css";
-import React, { useEffect } from 'react'; // <--- import the hook
+import React, { useEffect } from "react"; // <--- import the hook
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import {useRouter, Router} from 'next/router';
+import { useRouter, Router } from "next/router";
 
 declare global {
   interface Window {
@@ -13,42 +13,30 @@ declare global {
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
-  function pageView(url: string , title: string) {
+  function pageView(url: string, title: string) {
+    window &&
+      window.dataLayer &&
+      window.dataLayer.push({
+        event: "VirtualPageview",
+        virtualPageURL: url,
+        virtualPageTitle: title,
+      });
 
-    window && window.dataLayer && window.dataLayer.push({
-      'event': 'VirtualPageview',
-      'virtualPageURL': url,
-      'virtualPageTitle': title,
-  });
-
-
-    return (
-      console.log(title),
-      console.log(url)
-    )
+    return console.log(title), console.log(url);
   }
 
-
-   
-
   useEffect(() => {
-
     pageView(router.pathname, document.title);
 
-    const handleRouteChange = (url:string) => {
+    const handleRouteChange = (url: string) => {
       pageView(url, document.title);
-  };
-  Router.events.on('routeChangeComplete', handleRouteChange);
-  return () => {
-      Router.events.off('routeChangeComplete', handleRouteChange);
-  };
-}, []);
+    };
+    Router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      Router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
 
-
-
-   
-
-    
   return (
     <>
       <Head>
@@ -60,5 +48,3 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp;
-
-
--- pages/index.tsx
+++ pages/index.tsx
@@ -19,9 +19,9 @@ function Home() {
             <Image width={"400px"} src={Logo} alt="" />
             <h2 className="text-lg font-bold text-white sm:text-2xl md:text-3xl lg:text-4xl pt-2 pb-2">
               <span className="block">
-                Canadoctors es la primer plataforma que conecta   medicos y pacientes de cannabis medicinal.
+                Canadoctors es la primer plataforma que conecta medicos y
+                pacientes de cannabis medicinal.
               </span>
-             
             </h2>
             <div className="mt-10 flex flex-col sm:-mx-2 sm:flex-row sm:justify-center">
               <Link href="/doctorForm">
