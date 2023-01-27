bb1d6e7a9bf29d80fea920099c8fee8664348536
lucasbaz <b.lucasgabriel@gmail.com>
2023-01-27 20:03:12 -0300

    Refactoring Icons components and index

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,54 +1,29 @@
 import Image from "next/image";
-import icon from "../assets/images/icon.png";
-import iconLaw from "../assets/images/iconlaw.png";
-import iconBlocks from "../assets/images/iconblockchain.png";
 
-export const Icons = () => {
+
+type IconsProps = {
+  icon: any;
+  title1: string;
+  shortDesc: string;
+}
+
+export const Icons = ({
+  icon,
+  title1,
+  shortDesc,
+}: IconsProps) => {
   return (
     <div className="sm:flex flex-wrap justify-evenly items-center text-center gap-8">
       <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 px-4 py-4">
-        <div className="text-black  ">
+        <div className="text-black">
           <Image src={icon} width={150} height={150} />
           <div className="font-bold text-xl ml-6">
             {" "}
-            <h1> Profesionales</h1>
+            <h1>{title1}</h1>
           </div>
           <div className="ml-6">
             <p>
-              Conecte con medicos especialistas en tratamientos de diversas
-              patologías mediante cannabis medicinal
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 px-4 py-4">
-        <div className=" text-black  ">
-          <Image src={iconBlocks} width={150} height={150} />
-          <div className="font-bold text-xl ml-6">
-            {" "}
-            <h1>Blockchain</h1>
-          </div>
-          <div className="ml-6">
-            <p>
-              Cannadoctors genera una huella digital en la red de bloques
-              permitiendo validar la trazabilidad de los pacientes
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 px-4 py-4">
-        <div className=" text-black  ">
-          <Image src={iconLaw} width={150} height={150} />
-          <div className="font-bold text-xl ml-6">
-            {" "}
-            <h1> Cumplimiento</h1>
-          </div>
-          <div className="ml-6">
-            <p>
-              Nuestra plataforma cuenta con un proceso en cumplimiento con los
-              estandares de seguridad internacional
+              {shortDesc}
             </p>
           </div>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,32 @@ import { Footer } from "../components/Footer";
 import { ContactUs } from "../components/ContactUs";
 
 import background from "../assets/images/backgroundDoctors.jpg";
+import icon1 from "../assets/images/icon.png";
+import iconLaw from "../assets/images/iconlaw.png";
+import iconBlocks from "../assets/images/iconblockchain.png";
+
+interface IconsContent {
+  icon: any;
+  title1: string;
+  shortDesc: string;
+}
+
+const IconsContent = [
+  {
+    icon:{icon1},
+    title1:"Profesionales",
+    shortDesc:"Conecte con medicos especialistas en tratamientos de diversas patologías mediante cannabis medicinal"
+  },{
+    icon:{iconLaw},
+    title1:"Blockchain",
+    shortDesc:"Cannadoctors genera una huella digital en la red de bloques permitiendo validar la trazabilidad de los pacientes"
+  },{
+    icon:{iconBlocks},
+    title1:"Cumplimiento",
+    shortDesc:"Nuestra plataforma cuenta con un proceso en cumplimiento con los estandares de seguridad internacional"
+  }
+]
+
 function LandingPage() {
   return (
     <>
@@ -22,7 +48,14 @@ function LandingPage() {
         button1Title={"Registrate"}
         button2Title={"Inicial Sesión"}
       />
-      <Icons />
+      {IconsContent.map( icon => (
+          <Icons
+            icon={icon.icon}
+            title1={icon.title1}
+            shortDesc={icon.shortDesc} 
+          />
+        )
+      )}
       <Tech />
       <Movie />
       <Iphone />
