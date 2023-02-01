bccc3ef7d863c253d8d57a8925fbee17b6fab95b
jhonysalice <jhonysalice93@gmail.com>
2023-01-31 23:27:09 -0300

    Format change in video

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -1,12 +1,11 @@
 export const Movie = () => {
   return (
-    <div className="w-full flex justify-center items-center h-[100vh]">
+    <div className="w-full flex justify-center items-center h-[100vh] bg-gradient-to-b from-[#8CBB93]/90 to-[#00A099]/90 bg-opacity-40">
       <iframe
-        width="80%"
-        height="80%"
+        width="90%"
+        height="90%"
         src="https://www.youtube.com/embed/KowiQ_27A70"
         title="YouTube video player"
-        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
       ></iframe>
