b17e0131c3ececd2cb1ed8610100024cb7da9b93
jhonysalice <jhonysalice93@gmail.com>
2023-02-06 19:46:35 -0300

    LandingStyle style change

--- src/pages/landingSent/index.tsx
+++ src/pages/landingSent/index.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 
-import Logo from "../../../public/images/logosCD/logopositivo.svg";
+import Logo from "../../../public/images/logosCD/logocanadoctorWhite.svg";
 import iconEmail from "../../../public/images/iconCards/email.png";
 
 function LandingSent() {
   const { query } = useRouter();
 
   return (
-    <div className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden py-6 sm:py-12 h-full">
+    <div className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden py-6 sm:py-12 h-full bg-gradient-to-b from-[#8CBB93]/90 to-[#00A099]/90 bg-opacity-40">
       <div className="max-w-xl px-5 text-center">
         <div className="mb-6">
           <Image src={Logo} height={80} width={350} alt="" />
@@ -17,7 +17,7 @@ function LandingSent() {
         <div>
           <Image src={iconEmail} height={159} width={150} alt="" />
         </div>
-        <h2 className="mt-8 text-[30px] font-semibold text-[#5A5A5A]">
+        <h2 className="mt-8 text-[30px] font-semibold text-white">
           Gracias <span className="capitalize">{query.name}</span>, nos
           pondremos en contacto contigo a la brevedad.
         </h2>
