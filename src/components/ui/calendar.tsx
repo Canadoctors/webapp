99da4227a72dc0e65c6f254699e0609587fbceef
fedebais <federicomatiasbais@gmail.com>
2024-11-26 10:43:22 -0300

    add toast to pr docform

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
-import { DayPicker, Components } from "react-day-picker";
+import { DayPicker } from "react-day-picker";
 
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
@@ -13,12 +13,6 @@ function Calendar({
   showOutsideDays = true,
   ...props
 }: CalendarProps) {
-  // Custom components for navigation
-  const customComponents: Partial<Components> = {
-    IconLeft: () => <ChevronLeft className="h-4 w-4" />,
-    IconRight: () => <ChevronRight className="h-4 w-4" />,
-  };
-
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
@@ -57,7 +51,10 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
-      components={customComponents}
+      components={{
+        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
+        IconRight: () => <ChevronRight className="h-4 w-4" />,
+      }}
       {...props}
     />
   );
